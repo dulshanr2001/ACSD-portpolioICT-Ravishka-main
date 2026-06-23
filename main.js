15d633eedcacde99c92d3a862941afde6cc4cfc3
dulshanr2001 <dulshanr92@gmail.com>
2026-06-23 17:02:02 +0530

    I add typed.js functionality for dynamic text display in navbar

--- main.js
+++ main.js
@@ -5,3 +5,12 @@ menuIcon.onclick = () => {
     menuIcon.classList.toggle('fa-xmark'); // Toggles the X icon for closing the navbar
     navbar.classList.toggle('active');     // Toggles the active class for the navbar
 };
+
+/*============== Typed js ===============*/
+const typed = new Typed('.multiple-text', {
+    strings: [ 'Fullstack Developer', 'Web Developer', 'UI/UX Designer','Freelancer'], 
+    typeSpeed: 70,
+    backSpeed: 70,
+    backDelay: 1000,
+    loop: true 
+});
